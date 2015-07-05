66efdfe26176b97cd8f4a9b32e1b5c005cd64ce7
xhb1987 <xianghb1987@gmail.com>
2015-07-05 23:13:56 +0800

    add new angular js project which is the calendar app
    initialize the calendar app

---
+++ controllers/register/register.js
@@ -0,0 +1,5 @@
+var register = angular.module('register', [])
+
+register.controller('registerController', ['$http', '$location', function ($http, $location) {
+	console.log('test register controller');
+}])
---
+++ js/app.js
@@ -0,0 +1,45 @@
+﻿var app = angular.module("appCalender", [
+    'ngRoute',
+    'register']);
+
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.
+    when('/register', {
+        templateUrl: 'views/register/register.html',
+        controller: 'registerController'
+    }).
+    when('/index', {
+        templateUrl: 'views/index.html',
+        controller: 'indexController'
+    }).
+    otherwise({
+        redirectTo: '/index'
+    })
+}])
+
+app.controller('appController', function () {
+    console.log('app controller')
+})
+
+// Code goes here
+
+// var demo = angular.module('app', ['ngRoute']);
+
+// demo.config(function($routeProvider){
+//   $routeProvider.when('/', {
+//     controller: 'testController',
+//     templateUrl: 'test.html'
+//   })
+// })
+
+// var controllers = {};
+// controllers.testController = function($scope){
+//    $scope.first = "Info";
+//     $scope.customers=[
+//         {name:'jerry',city:'chicago'},
+//         {name:'tom',city:'houston'},
+//         {name:'enslo',city:'taipei'}
+//     ];
+// }
+
+// demo.controller(controllers)
